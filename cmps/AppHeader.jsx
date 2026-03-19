cec36d16f89526059145f99c313ce287d4501cf5
OmerSha23 <oshahar95@gmail.com>
2026-03-19 18:33:41 +0200

    Fixing code

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-const { Link, NavLink } = ReactRouterDOM
 
-export function AppHeader() {
-
-    return <header className="app-header">
-        <Link to="/">
-            <h3>LOGO ✨</h3>
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
-    </header>
-}
-=======
 const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
@@ -45,4 +28,4 @@ export function AppHeader() {
         </nav>
     </header>
 }
->>>>>>> 6f69f36 (Add css files for note app)
+
